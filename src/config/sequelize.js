1bcfa6061b9d40c5b84f7abd0658e05ebec3ce3c
ResiWicaksono <resiwicaksono98@gmail.com>
2022-01-24 10:16:28 +0700

    setting config database to heroku

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,10 +1,11 @@
 const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize({
-    database: 'todolist_sequelize',
-    host: 'localhost',
-    username: 'root',
-    password: 'password',
+    database: 'd5tfrdq2ehukrs',
+    host: 'ec2-34-194-171-47.compute-1.amazonaws.com',
+    username: 'fkfcmtaswviqnp',
+    port: 5432,
+    password: '9482a9a57b7a411f7c039aadb0771e15d3cd85a0787e8cb4c837ce2366fa45f2',
     dialect: 'mysql'
 })
 
